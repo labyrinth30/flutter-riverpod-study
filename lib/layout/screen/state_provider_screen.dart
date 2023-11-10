52c31742253bbcc21b328c5c5dc6997cba375a40
younha00 <ayeon0@office.skhu.ac.kr>
2023-11-10 15:30:26 +0900

    ref.watch

--- lib/layout/screen/state_provider_screen.dart
+++ lib/layout/screen/state_provider_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_study/layout/default_layout.dart';
+import 'package:flutter_riverpod_study/riverpod/state_provider_screen.dart';
 
 // StatelessWidget말고 ConsumerWidget을 사용
 class StateProviderScreen extends ConsumerWidget {
@@ -8,6 +9,8 @@ class StateProviderScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // ref.watch는 특정 프로바이더를 바라보고 있다가 그 프로바이더가 변경이 된다면은 빌드를 다시 실행해라
+    final provider = ref.watch(numberProvider);
     return const DefaultLayout(
       title: 'state_provider_screen',
       body: Column(
