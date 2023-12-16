be9f014905977501065e6041a7571c1aa7ddc247
younha00 <ayeon0@office.skhu.ac.kr>
2023-12-16 20:08:17 +0900

    stream provider

--- lib/screen/stream_provider_screen.dart
+++ lib/screen/stream_provider_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_study/layout/default_layout.dart';
 import 'package:flutter_riverpod_study/riverpod/stream_provider.dart';
