15ecc47b478d8febc3583552fcf96449cb732994
Lee, Younha <younha0088@gmail.com>
2023-12-25 23:54:23 +0900

    style: 코드 수정

--- lib/riverpod/state_provider.dart
+++ lib/riverpod/state_provider.dart
@@ -3,5 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // 숫자 상태관리
 // StateProvider의 파라미터에는 함수가 하나 들어감
 // 우리가 관리하고자 하는 값을 반환해주면 됨
-// Generic에는 관리할 값의 타입을 넣어줌
+// 제네릭에는 관리할 값의 타입을 넣어줌
 final numberProvider = StateProvider<int>((ref) => 0);
