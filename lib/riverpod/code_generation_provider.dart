2ba915e00782ab08e1d416b8a38c5f2e84dc6664
Lee, Younha <younha0088@gmail.com>
2023-12-27 00:59:50 +0900

    feat: KeepAlive

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -13,5 +13,21 @@ String gState(GStateRef ref) {
   return 'Hello  Code Generation';
 }
 
+// AutoDispose 키워드가 자동으로 생성됨
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 10;
+}
+
+@Riverpod(
+  // autoDispose가 호출되지 않음
+  keepAlive: true,
+)
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 10;
+}
+
 // 2) 파라미터를 일반 함수처럼 사용할 수 있도록
 
--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_study/layout/default_layout.dart';
@@ -10,11 +10,50 @@ class CodeGenerationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
     return DefaultLayout(
       title: 'CodeGenerationScreen',
-      body: Column(children: [
-        Text('State1: $state1'),
-      ]),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('State1: $state1'),
+          state2.when(
+            data: (data) {
+              return Text(
+                'State2: ${data.toString()}',
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (error, stackTrace) {
+              return Text(
+                error.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          state3.when(
+            data: (data) {
+              return Text(
+                'State3: ${data.toString()}',
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (error, stackTrace) {
+              return Text(
+                error.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
